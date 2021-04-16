dd7b677e446cbe017d1aede016e94b1ffe12225c
Stosstruppe <nx125@live.jp>
2021-04-16 17:34:06 +0900

    update

--- z80asm/pc80/hello/dev.bat
+++ z80asm/pc80/hello/dev.bat
@@ -1,4 +1,3 @@
 @echo off
 path %path%;C:\etc\z88dk\bin
-set ZCCCFG=C:\etc\z88dk\lib\config
 cmd
--- z80asm/pc80/hello/mk.bat
+++ z80asm/pc80/hello/mk.bat
@@ -4,10 +4,8 @@ z80asm -b -l %target%
 @if errorlevel 1 (
 	goto end
 )
-
 z88dk-appmake +pc88 --org 0xd000 -b %target%.bin
 @if errorlevel 1 (
 	goto end
 )
-
 :end
